7d9ee4221584025792fe0a624bbd90d20724da03
jibaru <ignacioruedaboada@hotmail.com>
2020-02-02 22:11:19 -0500

    General changes

--- Modulo_6/3_ejemplo_uso_retrollamadas_opengl.cpp
+++ Modulo_6/3_ejemplo_uso_retrollamadas_opengl.cpp
@@ -15,7 +15,21 @@ int main(int argc, char* argv[]){
 	glutInit(&argc, argv);
 
 	glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE);
-	
+	glutInitWindowsize(640, 480);
+	glutCreateWindow("Session #04 - Solar System");
+
+	// Definición de las funciones de retrollamada
+	glutDisplayFunc(display);
+	glutReshapeFunc(resize);
+
+	// Eg. update se ejecutará cuando el sistema
+	// capture un evento de teclado
+	// Signatura de glutKeyboardFunc
+	// void glutKeyboardFunc(void (*func))
+	// (unsigned char key, int x, int y));
+	glutKeyboardFunc(update);
+
+	glutMainLoop();
 
 	return 0;
 }
